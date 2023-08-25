96b3e7a90ca687b2df9802c4f3290e57f961bef1
wjbmattingly <wjbmattingly@gmail.com>
2023-08-25 04:28:40 -0400

    Commit Message:
    Refine Keyword Extractor:
    
    - Addressed [W008] warning by ensuring tokens have vectors before computing similarity.
    - Enhanced keyword quality:
      - Ignored tokens that are like numbers.
      - Improved n-gram extraction to discard partial phrases starting or ending with whitespace or punctuation.
      - Prioritized n-grams over individual tokens by suppressing tokens that are part of a top-ranked n-gram.
    - Implemented a set-based approach to automatically avoid duplicate keywords.
    
    This refinement aims to enhance keyword quality and ensure efficient computation, especially for large documents.

--- keyword_spacy/components.py
+++ keyword_spacy/components.py
@@ -15,18 +15,17 @@ class KeywordExtractor:
         self.strict = strict
 
     def valid_token(self, token):
-        return not (token.is_punct or token.is_stop)
+        return not (token.is_punct or token.is_stop or token.like_num or not token.has_vector)
 
     def __call__(self, doc):
-        token_values = []
+        token_values = set()
 
-        # if not self.strict:
-            # Calculate the cosine similarity for individual tokens
+        # Calculate the cosine similarity for individual tokens
         for token in doc:
             if self.valid_token(token):
                 token._.keyword_value = token.similarity(doc)
                 if not self.strict:
-                    token_values.append((token.text, token._.keyword_value))
+                    token_values.add((token.text, token._.keyword_value))
 
         # Calculate the cosine similarity for n-grams based on min_ngram and max_ngram
         for n in range(self.min_ngram, self.max_ngram + 1):
@@ -34,12 +33,16 @@ class KeywordExtractor:
                 ngram = doc[i:i+n]
                 if all(self.valid_token(token) for token in ngram):
                     similarity = ngram.similarity(doc)
-                    token_values.append((" ".join([token.text for token in ngram]), similarity))
+                    ngram_text = " ".join([token.text for token in ngram])
+                    token_values.add((ngram_text.strip(), similarity))
+                    # If ngram is ranked, suppress individual tokens from the ranking
+                    for token in ngram:
+                        token_values.discard((token.text, token._.keyword_value))
         
         # Sort based on similarity values
         sorted_tokens = sorted(token_values, key=lambda x: x[1], reverse=True)
         
-        # Extract top keywords
-        top_keywords = [token[0] for token in sorted_tokens[:self.top_n]]
+        # Extract top keywords, ensuring they don't start or end with punctuation or whitespace
+        top_keywords = [token[0] for token in sorted_tokens[:self.top_n] if not (token[0].startswith(('\n', ' ')) or token[0].endswith(('\n', ' ')))]
         doc._.keywords = top_keywords
         return doc
